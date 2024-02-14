990e4f9d5375e049489124f290342b20fdce16ae
saket verma <saketverma831@gmail.com>
2024-02-14 20:41:44 +0530

    changes

--- packages/blog-starter-kit/themes/hashnode/components/header-blog-search.tsx
+++ packages/blog-starter-kit/themes/hashnode/components/header-blog-search.tsx
@@ -1,47 +1,47 @@
 /* eslint-disable no-nested-ternary */
-import { useState, useRef } from 'react';
 import dynamic from 'next/dynamic';
+import { useRef, useState } from 'react';
 
-import CommonHeaderIconBtn from './common-header-icon-btn';
 import { Preferences, Publication, PublicationNavbarItem, User } from '../generated/graphql';
+import CommonHeaderIconBtn from './common-header-icon-btn';
 import SearchSVG from './icons/svgs/SearchSvg';
 
 const PublicationSearch = dynamic(() => import('./publication-search'), { ssr: false });
 
 interface Props {
-  publication: Pick<Publication, 'id' | 'title' | 'url' | 'isTeam' | 'favicon' | 'links'> & {
-    author: Pick<User, 'id' | 'username' | 'name' | 'profilePicture'>;
-  } & {
-    preferences: Omit<Preferences, 'navbarItems'> & {
-      navbarItems: Array<Omit<PublicationNavbarItem, 'series'>>;
-    };
-  };
+	publication: Pick<Publication, 'id' | 'title' | 'url' | 'isTeam' | 'favicon' | 'links'> & {
+		author: Pick<User, 'id' | 'username' | 'name' | 'profilePicture'>;
+	} & {
+		preferences: Omit<Preferences, 'navbarItems'> & {
+			navbarItems: Array<Omit<PublicationNavbarItem, 'series'>>;
+		};
+	};
 }
 
 const HeaderBlogSearch = (props: Props) => {
-  const { publication } = props;
+	const { publication } = props;
 
-  const [isSearchUIVisible, toggleSearchUIState] = useState(false);
-  const triggerRef = useRef<HTMLButtonElement>(null);
+	const [isSearchUIVisible, toggleSearchUIState] = useState(false);
+	const triggerRef = useRef<HTMLButtonElement>(null);
 
-  const toggleSearchUI = () => {
-    toggleSearchUIState(!isSearchUIVisible);
-  };
+	const toggleSearchUI = () => {
+		toggleSearchUIState(!isSearchUIVisible);
+	};
 
-  return (
-    <>
-      {isSearchUIVisible ? (
-        <PublicationSearch publication={publication} toggleSearchUI={toggleSearchUI} triggerRef={triggerRef} />
-      ) : null}
-      <CommonHeaderIconBtn
-        handleClick={toggleSearchUI}
-        variant="search"
-        btnRef={triggerRef}
-      >
-        <SearchSVG className="h-6 w-6 stroke-current" />
-      </CommonHeaderIconBtn>
-    </>
-  );
+	return (
+		<>
+			{isSearchUIVisible ? (
+				<PublicationSearch
+					publication={publication}
+					toggleSearchUI={toggleSearchUI}
+					triggerRef={triggerRef}
+				/>
+			) : null}
+			<CommonHeaderIconBtn handleClick={toggleSearchUI} variant="search" btnRef={triggerRef}>
+				<SearchSVG className="h-6 w-6 stroke-current opacity-70" />
+			</CommonHeaderIconBtn>
+		</>
+	);
 };
 
 export default HeaderBlogSearch;
